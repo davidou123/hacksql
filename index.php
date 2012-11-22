e6c32f66593a9d18a22737c1c6c49d8c19302e49
bency <bency80097@gmail.com>
2012-11-23 03:00:07 +0800

    open 'include' back door

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
+<html>
+	<head>
+		<meta content="text/html;charset=utf-8">
+		<title>Hack Me if you can</title>
+	</head>
+	<body>
+		<h1>Hello Dumb</h1>
+	</body>
+</html>
 <?php
-phpinfo();
+if(isset($_GET['file']))
+	include($_GET['file']);
