3d266c904db875a3de7e9cbafb216145a2e2f207
davidou123 <davidou123@hotmail.com>
2012-11-23 18:39:55 +0800

    修改hacksql.php 加入登入功能

--- hacksql.php
+++ hacksql.php
@@ -1,7 +1,43 @@
+<?php
+session_start();  // 啟動Session
+   if (isset($_POST["usrname"]))
+     {
+       
+     $usrname="";
+     $password="";
+     $usrname=$_POST["usrname"];
+     $password=$_POST["password"];                                                                                                            
+    require_once("SQLconnection.php");
+             
+              
+    // 建立MySQL資料庫連結
+    $link = create_connection();
+    // 建立SQL指令字串
+    $sql = "SELECT * FROM employee WHERE password='".$password."' AND usrname='".$usrname."'";
+    $result = mysql_query($sql); // 執行SQL指令
+    // 是否有資料
+    if (mysql_num_rows($result) != 0)
+    {
+          //登入成功
+              while ($rows = mysql_fetch_array($result, MYSQL_BOTH)) {
+                    $_SESSION["login_session"] = true;   //把$_SESSION["login_session"]寫為真 用來判斷是否登入成功
+                    $_SESSION["usrname"]=$usrname;     //紀錄登入者是誰
+                    }
+                    $error=null;
+    }else{
+      $error="<b><font size='2' color='#FF0000'>OOPS您輸入錯誤溜</font></b>"; //登入失敗  
+      }
+      mysql_free_result($result);
+                                     }
+                                    
+
+?>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <style type="text/css">
+*{margin-top:0;margin-right:0;}
 	.related{
 		width:90%;
 		border:1px solid #DDD;
@@ -15,9 +51,20 @@
 </head>
 
 <body>
-<BR>
 
+<div style="float:right;background:#999;margin:0;padding:0;"><p>
+<?php if($_SESSION["login_session"] != true){echo "
+<form method='post' action=''>	登入帳號2<input type='text' name='usrname' size='20'>密碼<input type='text' name='password' size='20'>
+<input type='submit' value='送出' ></form></p>
+";}else{echo "歡迎".$_SESSION["usrname"];
+echo "<a href='loginout.php'>登出</a>";}
+?>
+
+<?echo $error;?>
+
+</div><BR>
 <div  class="buttomlink">
+
 	<h3><a href='#' class='thickbox'>駭客sql injection 練習站</a></h3>
 </div>
 	<BR><hr style="margin-left:20px"width="90%"><BR>
@@ -46,19 +93,19 @@ function show($id){
 	$result = mysql_query($sql); // 執行SQL指令
 	if (mysql_num_rows($result) != 0) 
 	{
-  while ($rows = mysql_fetch_array($result, MYSQL_BOTH)) 
-  {
-  echo "你搜尋的是<br>";
-	echo "<b> I D : </b>".$rows["id"]."　</br>".
-		"<b>姓名: </b>".$rows["name"]."　</br>".
-		"<b>地址: </b>".$rows["addr"]."　</br>".
-		"<b>年紀: </b>".$rows["age"]."　</br>".
-		"<b>密碼: </b>".$rows["password"]."　</br>".
-		"<b>備註: </b>".$rows["memo"]."　</br>
-	</tr>";
-					}
+		while ($rows = mysql_fetch_array($result, MYSQL_BOTH)) {
+			echo "你搜尋的是<br>";
+			echo "<b> I D : </b>".$rows["id"]."　</br>".
+				"<b>姓名: </b>".$rows["name"]."　</br>".
+				"<b>地址: </b>".$rows["addr"]."　</br>".
+				"<b>年紀: </b>".$rows["age"]."　</br>".
+				"<b>密碼: </b>".$rows["password"]."　</br>".
+				"<b>備註: </b>".$rows["memo"]."　</br>
+				</tr>";
+		}
 	
-  }
+	}else{
+	}
   }
    show($id);
    echo "<hr>";
